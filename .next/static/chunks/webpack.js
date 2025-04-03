9dc909556775064db07530e11c10720f285e55da
Corentin Robert <corentinrobert@Mac-mini-de-Corentin.local>
2025-04-03 22:32:14 +0200

    Modification du chemin de l'image de profil pour utiliser JPG

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d432a9b434a38b98"; }
+/******/ 		__webpack_require__.h = function() { return "95004f7b02153d55"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.db987b74a38b04ba.hot-update.js
+++ .next/static/webpack/webpack.db987b74a38b04ba.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "d432a9b434a38b98"; }
+/******/ 	__webpack_require__.h = function() { return "95004f7b02153d55"; }
 /******/ }();
 /******/ 
 /******/ }
