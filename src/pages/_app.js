115df173d061e49d48e14c2cfab14629091a9f6d
Corentin Robert <corentinrobert@Mac-mini-de-Corentin.local>
2025-04-03 22:12:22 +0200

    Ajout de Vercel Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
 import { ThemeProvider } from 'next-themes'
 import '../styles/globals.css'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
       <SpeedInsights />
+      <Analytics />
     </ThemeProvider>
   )
 }
