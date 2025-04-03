e75264aa36d8b2cba078972287a08270567d7341
Corentin Robert <corentinrobert@Mac-mini-de-Corentin.local>
2025-04-03 21:41:45 +0200

    Ajout de Vercel Speed Insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import { ThemeProvider } from 'next-themes'
 import '../styles/globals.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
+      <SpeedInsights />
     </ThemeProvider>
   )
 }
