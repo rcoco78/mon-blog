d4aa693d445423f04996183a69677fe542682b0c
corent1robert <corentin.robert@hetic.net>
2025-12-29 10:19:38 +0100

    Mise à jour de l'ID Google Tag Manager (GTM-WPCV4F5G)

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5X8V3G7J');`
+})(window,document,'script','dataLayer','GTM-WPCV4F5G');`
           }}
         />
         {/* End Google Tag Manager */}
@@ -35,7 +35,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5X8V3G7J"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WPCV4F5G"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
